7ec2798cf9b13c81a8a5c91b621f6863d60655ba
jrvla <vladimir.artemj@gmail.com>
2026-04-30 01:47:40 +0500

    configured api for model

--- api/main.py
+++ api/main.py
@@ -1,16 +1,49 @@
 from transformers import BertForSequenceClassification, AutoTokenizer
-import torch
 import os
-import fastapi
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv, dotenv_values
+from api.dto.modelRequest import ModelRequest
+from api.dto.modelResponse import ModelResponse
+from api.services.predict import fit
 
-repo = "whoaasd/emotions-model"
+load_dotenv()
 
-# root_path = os.getcwd()
+logger = logging.getLogger(__name__)
 
-# model = BertForSequenceClassification.from_pretrained(f'{root_path}/models/model')
-# tokenizer = AutoTokenizer.from_pretrained(f'{root_path}/models/tokenizer')
+root_path = os.getcwd()
+env = os.getenv("ENVIRONMENT")
+repo = os.getenv("MODEL_REPO_NAME")
 
-model = BertForSequenceClassification.from_pretrained(repo)
-tokenizer = AutoTokenizer.from_pretrained(repo)
+if env == "DEV":
+    model_path = os.path.join(root_path, "models")
+else:
+    model_path = repo
 
-print(model)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.model = BertForSequenceClassification.from_pretrained(model_path)
+    app.state.tokenizer = AutoTokenizer.from_pretrained(model_path)
+    logger.log(level=0, msg="Service started")
+    yield
+    logger.info("Service shutting down...")
+
+app = FastAPI(
+    lifespan=lifespan,
+    title="Emotions recognition service API",
+    description="API for emotions recognition using fine tuned BERT model",
+    version="1.0.0"
+)
+
+@app.post('/predict', tags=["Emotion Model"], response_model=ModelResponse)
+async def predict(request: ModelRequest):
+    model = app.state.model
+    tokenizer = app.state.tokenizer
+    try:
+        output = fit(model=model, tokenizer=tokenizer, input=request.text)
+    except Exception:
+        logger.exception(msg="Model inference failed")
+        raise HTTPException(status_code=500, detail="Model inference failed")
+    
+    return ModelResponse(label=output['label'], confidence=output['confidence'])
